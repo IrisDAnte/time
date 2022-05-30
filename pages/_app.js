78faca5efef1560a183ba301459e724a5e1239b6
IrisDAnte <swapnilkumar616@gmail.com>
2022-05-30 16:24:11 +0600

    Initial Commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
+import dynamic from 'next/dynamic';
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default dynamic(() => Promise.resolve(MyApp), {
+  ssr: false,
+});
+
+// export default MyApp
