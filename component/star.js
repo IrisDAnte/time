2dff0a2e9bd356b59483764bd3d9c0edd018ee97
IrisDAnte <swapnilkumar616@gmail.com>
2022-06-02 18:08:43 +0600

    Improved falling star

--- component/star.js
+++ component/star.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import styles from '../styles/Star.module.css'
 
 const sleep = ms => new Promise(r => setTimeout(r, ms));
+const randomInt = (min, max) => { return Math.floor(Math.random() * (max - min + 1) + min) }
 
 export default function Star({ density, rate }) {
     useEffect(() => {
@@ -10,11 +11,11 @@ export default function Star({ density, rate }) {
             const limit = Math.floor(Math.sqrt(window.screen.height * window.screen.width) * density)
             if (el.length < limit) {
                 const target = el[el.length - 1].cloneNode(false)
-                target.style.top = `${Math.floor(Math.random() * 100)}vh`
-                target.style.left = `${Math.floor(Math.random() * 100)}vw`
-                target.style.opacity = Math.random() * (1 - 0.1) + 0.1
+                target.style.top = `${randomInt(0, 100)}vh`
+                target.style.left = `${randomInt(0, 100)}vw`
+                target.style.opacity = (Math.random() * (1 - 0.1) + 0.1).toFixed(1)
                 document.querySelector('#starContainer').appendChild(target)
-                await sleep(100 * Math.floor(Math.random() * 10))
+                await sleep(100 * randomInt(1, 10))
             } else if (el.length > limit && limit > 0) el[Math.floor(Math.random() * el.length)].remove()
         }
         const fallingStar = async () => {
@@ -22,18 +23,19 @@ export default function Star({ density, rate }) {
             const limit = Math.floor(1000 / rate)
             if (el.length < 10 && isFinite(limit)) {
                 const target = document.createElement('div')
-                target.style.setProperty('--origin', `${Math.floor(Math.random() * 100)}vmax`)
+                target.style.setProperty('--top', `${randomInt(0, 100)}vh`)
+                target.style.setProperty('--left', `${randomInt(0, 100)}vw`)
                 document.querySelector(`.${styles.fallingStarContainer}`).appendChild(target)
                 const animate = async () => {
-                    const tempDelay = limit * Math.floor(Math.random() * (10 - 1) + 1)
-                    const tempDuration = Math.floor(Math.random() * (30 - 10) + 10) * 100
+                    const tempDelay = limit * Math.floor(randomInt(1, 10))
+                    const tempDuration = Math.floor(randomInt(10, 30)) * 100
                     target.animate([
                         {
                             transform: 'rotate(-45deg) translateX(0)',
                             opacity: 1
                         },
                         {
-                            transform: 'rotate(-45deg) translateX(-1000vmax)',
+                            transform: 'rotate(-45deg) translateX(-1000vw)',
                             opacity: 0
                         }
                     ], {
